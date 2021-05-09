197330a7b79c05e62e17559c25fb2851db87477d
haeurum <80522886+haeurum@users.noreply.github.com>
2021-05-10 00:01:11 +0900

    modify week-6 code project

--- code/week-6/GNB/classifier.py
+++ code/week-6/GNB/classifier.py
@@ -33,7 +33,22 @@ class GNB():
         Collect the data and calculate mean and standard variation
         for each class. Record them for later use in prediction.
         '''
-        # TODO: implement code.
+
+        Train_data = {}
+
+        for class_name in self.classes :
+            Train_data[class_name] = []
+
+        for ind in range(len(Y)) :
+            label = Y[ind]
+            Train_data[label].append(X[ind])
+
+        self.class_Train = {label :{} for label in self.classes} 
+        for label_tgt in self.classes :
+            self.class_Train[label_tgt]['mean'] = np.mean(Train_data[label_tgt], 0)
+            self.class_Train[label_tgt]['std'] = np.std(Train_data[label_tgt], 0)
+                    
+        return self.class_Train
 
     # Given an observation (s, s_dot, d, d_dot), predict which behaviour
     # the vehicle is going to take using GNB.
@@ -45,6 +60,20 @@ class GNB():
         normalize them to get conditional probabilities.
         Return the label for the highest conditional probability.
         '''
-        # TODO: implement code.
-        return "keep"
+        probas = np.zeros(len(self.classes))
+        # Calculate Gaussian probability for each variable based on the
+        # mean and standard deviation calculated in the training process.
+        for ind_class in range(len(self.classes)):
+            proba = 1
 
+            # Multiply all the probabilities for variables, and then
+            # normalize them to get conditional probabilities.
+            for ind_obs in range(len(observation)) :
+                var_mean = self.class_Train[self.classes[ind_class]]['mean']
+                var_std = self.class_Train[self.classes[ind_class]]['std']
+                proba *= gaussian_prob(observation[ind_obs], var_mean[ind_obs], var_std[ind_obs])
+            probas[ind_class] = proba
+        res = probas / probas.sum()
+        
+        # Return the label for the highest conditional probability.
+        return self.classes[res.argmax(axis=0)]
